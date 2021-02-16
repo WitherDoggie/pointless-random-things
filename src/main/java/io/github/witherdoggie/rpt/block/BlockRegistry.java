ce0b799fe39a85a3e69dd56fdf41a071f7e91d74
Wither <elabont@siue.edu>
2021-02-15 20:59:02 -0600

    Add potato ore

--- src/main/java/io/github/witherdoggie/rpt/block/BlockRegistry.java
+++ src/main/java/io/github/witherdoggie/rpt/block/BlockRegistry.java
@@ -1,5 +1,7 @@
 package io.github.witherdoggie.rpt.block;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Material;
 import net.minecraft.block.MaterialColor;
@@ -13,10 +15,15 @@ import net.minecraft.util.registry.Registry;
 public class BlockRegistry {
 
     public static final SquishBlock SQUISH_BLOCK = new SquishBlock(AbstractBlock.Settings.of(Material.ORGANIC_PRODUCT, MaterialColor.LIME).sounds(BlockSoundGroup.SLIME));
+    public static final PotatoOre POTATO_ORE = new PotatoOre(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).sounds(BlockSoundGroup.STONE)
+            .requiresTool().strength(3.0f, 3.0f).breakByTool(FabricToolTags.PICKAXES, 0));
 
     public static void initializeBlocks(){
 
         Registry.register(Registry.BLOCK, new Identifier("rpt", "squish_block"), SQUISH_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("rpt","squish_block"), new BlockItem(SQUISH_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+
+        Registry.register(Registry.BLOCK, new Identifier("rpt", "potato_ore"), POTATO_ORE);
+        Registry.register(Registry.ITEM, new Identifier("rpt","potato_ore"), new BlockItem(POTATO_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 }
---
+++ src/main/java/io/github/witherdoggie/rpt/block/PotatoOre.java
@@ -0,0 +1,10 @@
+package io.github.witherdoggie.rpt.block;
+
+import net.minecraft.block.OreBlock;
+
+public class PotatoOre extends OreBlock {
+
+    public PotatoOre(Settings settings) {
+        super(settings);
+    }
+}
